188f1c25bc6bfbcecb66eb4b9e98136052a1c838
Miguel Frazão <miguel@miguel-HP-ProBook-450-G5>
2019-01-21 08:51:57 +0000

    adjusts

--- check_digits.py
+++ check_digits.py
@@ -12,7 +12,7 @@ def generate_ref(ent, ref_id, val):
 
 	# obrigar a ter sempre duas casas decimais, ex para que 2.00 nao fique 2.0 mas sim 2.00
 	val_display = '{0:.2f}'.format(val)
-	# remover decimal e preencher com zeros a esquerda para que tenha 8 chars
+	# remover decimal e preencher com zeros à esquerda para que tenha 8 chars
 	val_tmp = val_display.replace('.', '').zfill(8)
 
 	if(not val_tmp.isnumeric() or 1  > val > 1000000):
@@ -23,10 +23,10 @@ def generate_ref(ent, ref_id, val):
 		return {'error': 1, 'message': 'comprimento do num de controlo errado'}
 
 	prods = 0;
-	for key, value in enumerate(reversed(weights)):
-		prods += int(ctrl_num[key]) * value # // produto do num na posicao x pelo correspondente nos $weights revertido
+	for val1, val2 in zip(reversed(weights), map(int, ctrl_num)): # generator com [(51, 9), (73, 0), (17, 1), (89, 5), (38, 0), ....]
+		prods += val1 * val2 # // produto do num na posicao x pelo correspondente nos weights revertido
 
-	# obrigar a ter sempre dois digitos, colocar zero à esquerda se o resultado tiver apenas um digito
+	# obrigar a ter sempre dois digitos, colocar zero à esquerda se o resultado tiver apenas um dígito
 	check_digits = str(98-(prods%97)).zfill(2)
 	ref = '{}{}'.format(ref_id, check_digits)
 	return {'error': 0, 'message': {'ENT': ent, 'VAL': val_display.replace('.', ','), 'REF':  ref}}
