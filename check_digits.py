7995546af005db50bea5550e3c8009fdd399a350
Miguel Frazão <miguel@miguel-HP-ProBook-450-G5>
2019-01-21 14:05:40 +0000

    adjusts

--- check_digits.py
+++ check_digits.py
@@ -9,7 +9,7 @@
 
 def generate_ref(ent, ref_id, val):
 
-	weights = [51, 73, 17, 89, 38, 62, 45, 53, 15, 50, 5, 49, 34, 81, 76, 27, 90, 9, 30, 3];
+	weights = [51, 73, 17, 89, 38, 62, 45, 53, 15, 50, 5, 49, 34, 81, 76, 27, 90, 9, 30, 3]
 
 	# obrigar a ter sempre duas casas decimais, ex para que 2.00 nao fique 2.0 mas sim 2.00
 	val_display = '{:.2f}'.format(val)
@@ -22,7 +22,7 @@ def generate_ref(ent, ref_id, val):
 	if(len(base_num) != 20):
 		return {'error': 1, 'message': 'comprimento do num de controlo errado'}
 
-	prods = 0;
+	prods = 0
 	for val1, val2 in zip(weights, map(int, base_num)): # generator com [(51, 9), (73, 0), (17, 1), (89, 5), (38, 0), ....]
 		prods += val1 * val2 # // produto do num na posicao x pelo correspondente nos weights
 
@@ -31,7 +31,7 @@ def generate_ref(ent, ref_id, val):
 	ref = '{}{}'.format(ref_id, check_digits)
 	return {'error': 0, 'message': {'ENT': ent, 'VAL': val_display.replace('.', ','), 'REF':  ref}}
 
-ent = '90150';
-ref_id = '1231234';
-val = 432.11;
-print(generate_ref(ent, ref_id, float(val))); # {'message': {'REF': '123123451', 'ENT': '90150', 'VAL': '432,11'}, 'error': 0}
+ent = '90150'
+ref_id = '1231234'
+val = 432.11
+print(generate_ref(ent, ref_id, float(val))) # {'message': {'REF': '123123451', 'ENT': '90150', 'VAL': '432,11'}, 'error': 0}
